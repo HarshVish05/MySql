2b4af10375c125ed59d02c81837abfd0c3290cce
harsh <codeharshit05@gmail.com>
2023-05-08 17:56:27 +0530

    more practice

--- queries.sql
+++ queries.sql
@@ -29,3 +29,73 @@ select * from customers where phone is not Null;
 
 -- select * from customers where order by state desc, first_name asc; 
 select * from customers limit 5,4;
+
+
+-- join
+
+select * from orders o
+join customers c 
+on c.customer_id=o.customer_id;
+
+select order_id , c.customer_id , first_name from orders o
+join customers c on c.customer_id=o.customer_id;
+
+select order_id , c.customer_id , first_name,points from orders o
+join customers c on c.customer_id=o.customer_id
+where points>1000;
+
+-- joining b/w tabes from two diff databases
+select * from order_items oi
+join sql_inventory.products p
+on oi.product_id=p.product_id;
+
+select distinct oi.product_id,name,quantity_in_stock from order_items oi
+join sql_inventory.products p
+on oi.product_id=p.product_id
+where quantity_in_stock>50;
+
+-- self join 
+select e.employee_id,e.first_name,m.first_name as manager,m.employee_id from employees e
+join employees m
+on e.reports_to = m.employee_id;
+
+
+
+-- copy of a table
+create table new_orders as select * from orders;
+
+
+INSERT INTO `customers` VALUES (default,'harsh','vishwakarma','1986-03-05','781-532-6754','patal lok','patal','nk',default);
+select * from customers;
+
+update customers
+set points = 1575
+-- and  birth_date ='1990-05-02'
+where customer_id =12;
+
+delete from customers where customer_id =11;
+
+-- sub-queries
+select * from customers where customer_id in ( select customer_id from customers where points>2000 );
+
+-- union
+select order_id,order_date,
+'Active' as Status from orders
+where order_date>='2019-01-01'
+union
+select order_id,order_date,
+'Archived' as Status from orders
+where order_date>='2019-01-01';
+
+-- alter table
+alter table customers
+add Email varchar(100);
+
+update customers 
+set Email = 'xyz@gmail.com'
+where customer_id=12;
+
+select * from customers;
+
+
+
